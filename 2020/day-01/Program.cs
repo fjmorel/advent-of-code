297dadf4d439cdd6997486f3b86d017acae39275
Fred Morel <github@fmorel.net>
2020-12-04 22:51:56 -0500

    Completely LINQ day 1

--- 2020/day-01/Program.cs
+++ 2020/day-01/Program.cs
@@ -2,42 +2,21 @@
 using System.IO;
 using System.Linq;
 
-
-// #region Part 1
-
-// var lines = File.ReadAllLines("input.txt");
-// var nums = lines.Select(x => int.Parse(x)).ToList();
-
-// for (var i = 0; i < nums.Count; i++)
-// {
-// 	for (var j = 0; j < nums.Count; j++)
-// 	{
-// 		if (nums[i] + nums[j] == 2020 && i != j)
-// 		{
-// 			Console.WriteLine(nums[i] * nums[j]);
-// 		}
-// 	}
-// }
-
-// #endregion
-
-#region Part 2
-
 var lines = File.ReadAllLines("input.txt");
 var nums = lines.Select(x => int.Parse(x)).ToList();
 
-for (var i = 0; i < nums.Count; i++)
-{
-	for (var j = 0; j < nums.Count; j++)
-	{
-		for (var k = 0; k < nums.Count; k++)
-		{
-			if (nums[i] + nums[j] + nums[k] == 2020 && i != j && i != k && j != k)
-			{
-				Console.WriteLine(nums[i] * nums[j] * nums[k]);
-			}
-		}
-	}
-}
+var two = nums
+.SelectMany(x => nums.Where(y => y != x).Select(y => new[] { x, y }))
+.Where(list => list.Sum() == 2020 && list.Distinct().Count() == list.Count())
+.Select(list => list.Aggregate(1, (acc, x) => acc * x))
+.First();
 
-#endregion
+var three = nums
+.SelectMany(x => nums.Where(y => y != x).Select(y => new[] { x, y }))
+.SelectMany(list => nums.Where(z => !list.Contains(z)).Select(z => new[] { list[0], list[1], z }))
+.Where(list => list.Sum() == 2020 && list.Distinct().Count() == list.Count())
+.Select(list => list.Aggregate(1, (acc, x) => acc * x))
+.First();
+
+Console.WriteLine(two);
+Console.WriteLine(three);
