f865ed99ba11978652564df8625291ce554efc30
Fred Morel <github@fmorel.net>
2020-12-04 21:53:47 -0500

    Day 3. That's a lot of trees.

---
+++ 2020/day-03/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+var lines = System.IO.File.ReadAllLines("input.txt");
+var width = lines[0].Length;
+
+Console.WriteLine(new[]
+{
+	HowManyHits(1, 1),
+	HowManyHits(3, 1),
+	HowManyHits(5, 1),
+	HowManyHits(7, 1),
+	HowManyHits(1, 2),
+}.Aggregate((long)1, (acc, x) => acc * x));
+
+long HowManyHits(int right, int down)
+{
+	long treesHit = 0;
+	for (var i = 0; (i * down) < lines.Length; i++)
+	{
+		var symbol = lines[i * down][(i * right) % width];
+		if (symbol == '#')
+			treesHit++;
+	}
+	Console.WriteLine(treesHit);
+	return treesHit;
+}
