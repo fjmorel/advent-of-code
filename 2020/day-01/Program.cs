8239e780d8a6aeb7542fa6c98409af3985dacf94
Fred Morel <github@fmorel.net>
2020-12-04 21:11:23 -0500

    Awful day 1 solution

--- 2020/day-01/Program.cs
+++ 2020/day-01/Program.cs
@@ -1,3 +1,43 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
 
-Console.WriteLine("Hello World!");
+
+// #region Part 1
+
+// var lines = File.ReadAllLines("input.txt");
+// var nums = lines.Select(x => int.Parse(x)).ToList();
+
+// for (var i = 0; i < nums.Count; i++)
+// {
+// 	for (var j = 0; j < nums.Count; j++)
+// 	{
+// 		if (nums[i] + nums[j] == 2020 && i != j)
+// 		{
+// 			Console.WriteLine(nums[i] * nums[j]);
+// 		}
+// 	}
+// }
+
+// #endregion
+
+#region Part 2
+
+var lines = File.ReadAllLines("input.txt");
+var nums = lines.Select(x => int.Parse(x)).ToList();
+
+for (var i = 0; i < nums.Count; i++)
+{
+	for (var j = 0; j < nums.Count; j++)
+	{
+		for (var k = 0; k < nums.Count; k++)
+		{
+			if (nums[i] + nums[j] + nums[k] == 2020 && i != j && i != k && j != k)
+			{
+				Console.WriteLine(nums[i] * nums[j] * nums[k]);
+			}
+		}
+	}
+}
+
+#endregion
