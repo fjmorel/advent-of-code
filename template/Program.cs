18f78d1380ed2e2ea2bcb0f73212eae4623b1665
Fred Morel <github@fmorel.net>
2020-12-04 23:36:58 -0500

    Set up generic template

---
+++ template/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+var part1 = 0;
+var part2 = 0;
+
+foreach (var line in System.IO.File.ReadLines("input.txt"))
+{
+}
+
+Console.WriteLine(part1);
+Console.WriteLine(part2);
