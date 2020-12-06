dd1a4ae4312a065ee91367241dba902c6fc744fe
Fred Morel <github@fmorel.net>
2020-12-06 17:04:45 -0500

    Simplified Day 6

--- 2020/day-06/Program.cs
+++ 2020/day-06/Program.cs
@@ -5,41 +5,19 @@ using System.Linq;
 var part1 = 0;
 var part2 = 0;
 
-var charAnswers = new Dictionary<char, int>();
-var groupSize = 0;
-ResetGroup();
-foreach (var line in System.IO.File.ReadLines("input.txt"))
+var data = string.Join('\n', System.IO.File.ReadAllLines("input.txt"))
+	.Split("\n\n")
+	.Select(x => x.Split('\n'));
+foreach (var group in data)
 {
-	if (string.IsNullOrEmpty(line))
-	{
-		CountGroup();
-		ResetGroup();
-	}
-	else
-	{
-		groupSize++;
+	var charAnswers = new Dictionary<char, int>();
+	foreach (var line in group)
 		foreach (var ch in line)
-		{
-			charAnswers[ch] = charAnswers[ch] + 1;
-		}
-	}
+			charAnswers[ch] = charAnswers.GetValueOrDefault(ch) + 1;
+
+	part1 += charAnswers.Count();
+	part2 += charAnswers.Count(x => x.Value == group.Count());
 }
-CountGroup();
 
 Console.WriteLine(part1);
 Console.WriteLine(part2);
-
-void ResetGroup()
-{
-	groupSize = 0;
-	for (var ch = 'a'; ch <= 'z'; ch++)
-	{
-		charAnswers[ch] = 0;
-	}
-}
-
-void CountGroup()
-{
-	part1 += charAnswers.Count(x => x.Value > 0);
-	part2 += charAnswers.Count(x => x.Value == groupSize);
-}
