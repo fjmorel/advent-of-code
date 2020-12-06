7df79d02cf10aa43a71521f8a8bb048e71911e71
Fred Morel <github@fmorel.net>
2020-12-06 00:13:18 -0500

    Day 6

---
+++ 2020/day-06/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+var part1 = 0;
+var part2 = 0;
+
+var anyChar = new HashSet<char>();
+var byCharCounts = new Dictionary<char, int>();
+var group = 0;
+ResetGroup();
+foreach (var line in System.IO.File.ReadLines("input.txt"))
+{
+	if (string.IsNullOrEmpty(line))
+	{
+		part1 += anyChar.Count;
+		part2 += byCharCounts.Where(x => x.Value == group).Count();
+		ResetGroup();
+		group = 0;
+	}
+	else
+	{
+		group++;
+		foreach (var ch in line)
+		{
+			anyChar.Add(ch);
+			byCharCounts[ch] = byCharCounts[ch] + 1;
+		}
+	}
+}
+
+part1 += anyChar.Count;
+part2 += byCharCounts.Where(x => x.Value == group).Count();
+
+Console.WriteLine(part1);
+Console.WriteLine(part2);
+
+void ResetGroup()
+{
+	anyChar.Clear();
+	for (var ch = 'a'; ch <= 'z'; ch++)
+	{
+		byCharCounts[ch] = 0;
+	}
+}
