b70f134d5f640e529e441f90c5c0e4a25ba611ca
Fred Morel <github@fmorel.net>
2020-12-07 00:25:32 -0500

    Really awful part 1 s olution

---
+++ 2020/day-07/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+#nullable enable
+
+var part1 = 0;
+var part2 = 0;
+
+Dictionary<string, BagColor> rules = new();
+
+var data = System.IO.File.ReadAllLines("input.txt").Select(x => x.Split(" contain "));
+foreach (var group in data)
+{
+	var primary = group[0].Substring(0, group[0].IndexOf(" bags"));
+	Console.WriteLine(primary);
+	var contents = group[1] switch
+	{
+		"no other bags." => new List<BagColor>(),
+		string s => ParseContents(s),
+	};
+	rules[primary] = new BagColor(primary, 0, contents);
+	Console.WriteLine(rules[primary]);
+}
+
+var searched = new HashSet<string>();
+var matches = new HashSet<string>();
+
+FindColorsContainingColor("shiny gold");
+Console.WriteLine(matches.Count());
+Console.WriteLine(part2);
+
+void FindColorsContainingColor(string color)
+{
+	foreach (var result in rules.Where(x => x.Value.contents.Any(x => x.color == color)).Select(x => x.Key))
+	{
+		matches.Add(result);
+		if (!searched.Contains(result))
+		{
+			FindColorsContainingColor(result);
+		}
+	}
+}
+
+List<BagColor> ParseContents(string contents)
+{
+	var list = new List<BagColor>();
+	var pieces = contents.Split(", ");
+	foreach (var bag in pieces)
+	{
+		var color = bag.Replace(" bags", "").Replace(" bag", "").Replace(".", "");
+		var num = int.Parse(color.Substring(0, color.IndexOf(' ')));
+		color = color.Substring(color.IndexOf(' ') + 1);
+		var subrule = new BagColor(color, num, new List<BagColor>());
+		Console.WriteLine(subrule);
+		list.Add(subrule);
+	}
+
+	return list;
+}
+
+record BagColor(string color, int count, List<BagColor> contents);
