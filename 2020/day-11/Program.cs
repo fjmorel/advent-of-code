ef2a71fb59ea2c9bb495f8cf2dc50c1336253536
Fred Morel <github@fmorel.net>
2020-12-11 19:00:40 -0500

    LINQ all the things! Day 11

--- 2020/day-11/Program.cs
+++ 2020/day-11/Program.cs
@@ -3,120 +3,81 @@ using System.Collections.Generic;
 using System.Linq;
 using static System.Console;
 
-var prev = System.IO.File.ReadLines("input.txt").Select(x => x.Select(ParseChar).ToArray()).ToArray();
-var size = new Seat(prev.Length, prev[0].Length);
+var read = System.IO.File.ReadLines("input.txt").Select(x => x.Select(ParseChar).ToArray()).ToArray();
+var size = new Position(read.Length, read[0].Length);
 
-WriteLine(FindSteadyState(prev, size, 4, false));
-WriteLine(FindSteadyState(prev, size, 5, true));
+WriteLine(FindSteadyState(read, size, 4, false));
+WriteLine(FindSteadyState(read, size, 5, true));
 
-static int FindSteadyState(State[][] prev, Seat size, int threshold, bool lineOfSight)
+static int FindSteadyState(State[][] read, Position size, int threshold, bool lineOfSight)
 {
-	var cur = Iterate(prev, size, threshold, lineOfSight);
-	while (!Equal(prev, cur))
+	var write = Iterate(read, size, threshold, lineOfSight);
+	while (!read.SelectMany(x => x).SequenceEqual(write.SelectMany(x => x)))
 	{
-		prev = cur;
-		cur = Iterate(prev, size, threshold, lineOfSight);
+		read = write;
+		write = Iterate(read, size, threshold, lineOfSight);
 	}
-	return cur.SelectMany(x => x.Where(y => y == State.Filled)).Count();
+	return write.Sum(x => x.Count(y => y == State.Occupied));
 }
 
-static State[][] Iterate(State[][] prev, Seat size, int threshold, bool lineOfSight)
-{
-	var cur = CloneArray(prev);
-	for (var i = 0; i < size.i; i++)
+static State[][] Iterate(State[][] read, Position size, int threshold, bool lineOfSight)
+	=> read.Select((row, i) => row.Select((State, j) => FindNewState(new Position(i, j), read, size, threshold, lineOfSight)).ToArray()).ToArray();
+
+static State FindNewState(Position seat, State[][] read, Position size, int threshold, bool lineOfSight)
+	=> read[seat.i][seat.j] switch
 	{
-		for (var j = 0; j < size.j; j++)
-		{
-			cur[i][j] = prev[i][j] switch
-			{
-				State.Floor => State.Floor,
-				State.Empty => MaybeFill(new Seat(i, j), size, prev, lineOfSight),
-				State.Filled => MaybeClear(new Seat(i, j), size, prev, threshold, lineOfSight),
-				_ => throw new NotSupportedException(),
-			};
-		}
-	}
+		State.Floor => State.Floor,
+		State.Empty => MaybeFill(seat, size, read, lineOfSight),
+		State.Occupied => MaybeClear(seat, size, read, threshold, lineOfSight),
+		_ => throw new NotSupportedException(),
+	};
 
-	return cur;
-}
+static State MaybeClear(Position seat, Position size, State[][] read, int threshold, bool lineOfSight)
+	=> GetSurroundingPositions(seat, size, read, lineOfSight)
+		.Where(adj => read[adj.i][adj.j] == State.Occupied)
+		.Count() >= threshold ? State.Empty : State.Occupied;
 
-static State MaybeClear(Seat coord, Seat size, State[][] prev, int threshold, bool lineOfSight)
-	=> GetSurroundingPositions(coord, size, prev, lineOfSight)
-		.Where(adj => prev[adj.i][adj.j] == State.Filled)
-		.Count() >= threshold ? State.Empty : State.Filled;
+static State MaybeFill(Position seat, Position size, State[][] read, bool lineOfSight)
+	=> GetSurroundingPositions(seat, size, read, lineOfSight)
+		.Any(adj => read[adj.i][adj.j] == State.Occupied)
+		? State.Empty : State.Occupied;
 
-static State MaybeFill(Seat coord, Seat size, State[][] prev, bool lineOfSight)
-	=> GetSurroundingPositions(coord, size, prev, lineOfSight)
-		.Any(adj => prev[adj.i][adj.j] == State.Filled)
-		? State.Empty : State.Filled;
-
-static IEnumerable<Seat> GetSurroundingPositions(Seat coord, Seat size, State[][] prev, bool lineOfSight)
+static IEnumerable<Position> GetSurroundingPositions(Position coord, Position size, State[][] prev, bool lineOfSight)
 {
+	IEnumerable<Position> seats = new List<Position>()
+	{
+		new Position(-1, -1), new Position(-1, 0), new Position(-1, 1),
+		new Position(0, -1), new Position(0, 1),
+		new Position(1, -1), new Position(1, 0), new Position(1, 1),
+	};
 	if (!lineOfSight)
-		return GetIncrements()
-			.Select(x => coord + x)
-			.Where(x => x.IsWithin(size));
-
-	var seats = new List<Seat>();
-	foreach (var inc in GetIncrements())
-	{
-		var adj = coord + inc;
-		while (adj.IsWithin(size) && prev[adj.i][adj.j] == State.Floor)
-			adj = adj + inc;
-
-		if (adj.IsWithin(size))
-			seats.Add(adj);
-	}
-	return seats;
+		seats = seats.Select(x => coord + x);
+	else
+		seats = seats.Select(inc => FindLineOfSight(coord, size, prev, inc));
+	return seats.Where(x => x.IsWithin(size));
 }
 
-static List<Seat> GetIncrements() => new List<Seat>()
+static Position FindLineOfSight(Position coord, Position size, State[][] prev, Position inc)
 {
-	new Seat(-1, -1),
-	new Seat(-1, 0),
-	new Seat(-1, 1),
-	new Seat(0, -1),
-	new Seat(0, 1),
-	new Seat(1, -1),
-	new Seat(1, 0),
-	new Seat(1, 1),
-};
-
-static bool Equal(State[][] prev, State[][] cur)
-{
-	var width = cur[0].Length;
-	var height = cur.Length;
-	for (var i = 0; i < height; i++)
-	{
-		for (var j = 0; j < width; j++)
-		{
-			if (prev[i][j] != cur[i][j])
-				return false;
-		}
-	}
-	return true;
+	var adj = coord + inc;
+	while (adj.IsWithin(size) && prev[adj.i][adj.j] == State.Floor)
+		adj = adj + inc;
+	return adj;
 }
-
-static State[][] CloneArray(State[][] grid) => grid.Select(x => (State[])x.Clone()).ToArray();
 
 static State ParseChar(char letter) => letter switch
 {
 	'.' => State.Floor,
 	'L' => State.Empty,
-	'#' => State.Filled,
+	'#' => State.Occupied,
 	_ => throw new NotSupportedException(),
 };
 
-enum State
-{
-	Floor,
-	Empty,
-	Filled,
-}
+enum State { Floor, Empty, Occupied }
 
-record Seat(int i, int j)
+record Position(int i, int j)
 {
-	public static Seat operator +(Seat a, Seat b) => new Seat(a.i + b.i, a.j + b.j);
+	public static Position operator +(Position a, Position b) => new Position(a.i + b.i, a.j + b.j);
 
-	public bool IsWithin(Seat size) => this.i >= 0 && this.i < size.i && this.j >= 0 && this.j < size.j;
+	public bool IsWithin(Position size) => this.i >= 0 && this.i < size.i && this.j >= 0 && this.j < size.j;
 }
