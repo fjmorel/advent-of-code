817d8eb162d8ac1293857867f29c9ca7e0de851b
Fred Morel <github@fmorel.net>
2020-12-12 08:58:31 -0500

    Start part 2, trim part 1

--- 2020/day-12/Program.cs
+++ 2020/day-12/Program.cs
@@ -1,18 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Drawing;
 using System.Linq;
 using static System.Console;
 
 var read = System.IO.File.ReadLines("input.txt").Select(x => (x[0], int.Parse(x.Substring(1)))).ToList();
-var vector = new Vector(Direction.E, 0, 0);
 
-foreach (var line in read)
-{
-	vector = GetNewPosition(line.Item1, line.Item2, vector);
-}
-
+var vector = read.Aggregate(new Vector(Direction.E, 0, 0), (vector, line) => GetNewPosition(line.Item1, line.Item2, vector));
 WriteLine(Math.Abs(vector.X) + Math.Abs(vector.Y));
+
+var waypoint = new WaypointNav(0, 0, 10, 1);
+foreach (var line in read)
+	waypoint = GetNewPositionWaypoint(line.Item1, line.Item2, waypoint);
+WriteLine(Math.Abs(waypoint.ShipX) + Math.Abs(waypoint.ShipY));
 
 Vector GetNewPosition(char dir, int adj, Vector vector) => dir switch
 {
@@ -22,15 +21,38 @@ Vector GetNewPosition(char dir, int adj, Vector vector) => dir switch
 	'E' => vector with { X = vector.X + adj },
 	'W' => vector with { X = vector.X - adj },
 
-	'L' => vector with { Direction = GetNewDirection(vector.Direction, adj) },
-	'R' => vector with { Direction = GetNewDirection(vector.Direction, 360 - adj) },
+	'L' => vector with { Direction = (Direction)(((int)vector.Direction + adj) % 360) },
+	'R' => vector with { Direction = (Direction)(((int)vector.Direction + 360 - adj) % 360) },
 
 	'F' => GetNewPosition(vector.Direction.ToString()[0], adj, vector),
 	_ => throw new NotSupportedException(),
 };
 
-Direction GetNewDirection(Direction current, int adj) => (Direction)(((int)current + adj) % 360);
+WaypointNav GetNewPositionWaypoint(char dir, int adj, WaypointNav pt) => dir switch
+{
+	'N' => pt with { WaypointY = pt.WaypointY + adj },
+	'S' => pt with { WaypointY = pt.WaypointY - adj },
+
+	'E' => pt with { WaypointX = pt.WaypointX + adj },
+	'W' => pt with { WaypointX = pt.WaypointX - adj },
+
+	'L' => TurnWaypoint(pt, adj),
+	'R' => TurnWaypoint(pt, 360 - adj),
+
+	'F' => pt with { ShipX = pt.ShipX + adj * pt.WaypointX, ShipY = pt.ShipY + adj * pt.WaypointY },
+	_ => throw new NotSupportedException(),
+};
+
+WaypointNav TurnWaypoint(WaypointNav pt, int adj) => adj switch
+{
+	0 => pt,
+	90 => pt with { WaypointX = -pt.WaypointY, WaypointY = pt.WaypointX },
+	180 => pt with { WaypointX = -pt.WaypointX, WaypointY = pt.WaypointY },
+	270 => pt with { WaypointX = pt.WaypointY, WaypointY = -pt.WaypointX },
+	_ => throw new NotSupportedException(),
+};
 
 enum Direction { E = 0, N = 90, W = 180, S = 270 }
 
 record Vector(Direction Direction, int X, int Y);
+record WaypointNav(int ShipX, int ShipY, int WaypointX, int WaypointY);
