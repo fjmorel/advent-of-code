fafb6db93f965412c0ba35a3e68ea33802a010a1
Fred Morel <github@fmorel.net>
2020-12-13 00:32:42 -0500

    Slow brute force solution for part 2

--- 2020/day-13/Program.cs
+++ 2020/day-13/Program.cs
@@ -7,17 +7,23 @@ using static System.Console;
 var read = System.IO.File.ReadAllLines("input.txt");
 var earliest = int.Parse(read[0]);
 var buses = read[1].Split(',').Where(x => x != "x").Select(x => int.Parse(x)).ToList();
+var indexedBuses = read[1].Split(',').Select((id, i) => (id, i)).Where(x => x.id != "x").Select(x => (id: long.Parse(x.id), x.i)).ToList();
 var times = new Dictionary<int, int>();
 
 foreach (var bus in buses)
-{
-	times[bus] = FindEarliestAfter(earliest, bus);
-}
-
+	times[bus] = (earliest / bus + 1) * bus;
 var kv = times.OrderBy(x => x.Value).First();
 WriteLine($"Bus {kv.Key}. Wait {kv.Value - earliest}. Solution: {kv.Key * (kv.Value - earliest)}");
 
-int FindEarliestAfter(int earliest, int bus)
+var found = false;
+var biggest = indexedBuses.OrderByDescending(x => x.id).First();
+var interval = biggest.id;
+var offset = biggest.i;
+for (long timestamp = interval - offset; !found; timestamp += interval)
 {
-	return (earliest / bus + 1) * bus;
+	if (indexedBuses.All(x => (timestamp + x.i) % x.id == 0))
+	{
+		WriteLine(timestamp);
+		found = true;
+	}
 }
