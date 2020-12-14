ca968b3fe724612f6c5e2e62f81db97414401b72
Fred Morel <github@fmorel.net>
2020-12-14 01:41:42 -0500

    Simplify Day 14

--- 2020/day-14/Program.cs
+++ 2020/day-14/Program.cs
@@ -6,76 +6,62 @@ using static System.Console;
 
 var read = System.IO.File.ReadAllLines("input.txt");
 
-Part1(read);
-WriteLine("");
-Part2(read);
+WriteLine(Part1(read));
+WriteLine(Part2(read));
 
-void Part1(string[] read)
+long Part1(string[] read)
 {
-	var memory = new Dictionary<long, string>();
-	ReadOnlySpan<char> mask = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+	var memory = new Dictionary<long, char[]>();
+	var mask = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
 	foreach (var line in read)
 	{
 		var pieces = line.Split(" = ");
 		if (pieces[0] == "mask")
 		{
-			mask = pieces[1];//mask.ToArray().Zip(pieces[1]).Select(x => x.Second == 'X' ? x.First : x.Second).ToArray();
+			mask = pieces[1];
 		}
 		else
 		{
-			var index = int.Parse(pieces[0][4..^1]);
-			var value = int.Parse(pieces[1]);
-			var str = Convert.ToString(value, 2).PadLeft(mask.Length, '0');
-			memory[index] = new string(str.Zip(mask.ToArray()).Select(x => x.Second != 'X' ? x.Second : x.First).ToArray());
+			var valueStr = ToBinary(int.Parse(pieces[1]));
+			memory[int.Parse(pieces[0][4..^1])] = valueStr.Zip(mask).Select(x => x.Second != 'X' ? x.Second : x.First).ToArray();
 		}
 	}
 
-	WriteLine(memory.Select(x => Convert.ToInt64(x.Value.ToString(), 2)).Sum());
+	return memory.Sum(x => FromBinary(new string(x.Value)));
 }
 
-void Part2(string[] read)
+long Part2(string[] read)
 {
-	var mask = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
-	var zero = string.Intern(Convert.ToString(0, 2).PadLeft(mask.Length, '0'));
+	var masks = new List<char[]>();
 	var memory = new Dictionary<long, long>();
 	foreach (var line in read)
 	{
 		var pieces = line.Split(" = ");
 		if (pieces[0] == "mask")
 		{
-			mask = pieces[1];//mask.ToArray().Zip(pieces[1]).Select(x => x.Second == 'X' ? x.First : x.Second).ToArray();
+			masks = GeneratePlainMasks(pieces[1]);
 		}
 		else
 		{
-			var index = int.Parse(pieces[0][4..^1]);
-			var value = int.Parse(pieces[1]);
-			var valueStr = Convert.ToString(value, 2).PadLeft(mask.Length, '0');
-			var baseAddress = Convert.ToString(index, 2).PadLeft(mask.Length, '0');
-			var masks = GeneratePlainMasks(mask);
-			var addresses = masks.Select(x => ApplyMaskToAddress(baseAddress, x)).ToList();
-			foreach (var address in addresses)
-				memory[address] = value;
-
-			//memory[index] = new string(str.Zip(mask.ToArray()).Select(x => x.Second == '0' ? x.First : x.Second : x.First).ToArray());
+			var baseAddress = ToBinary(int.Parse(pieces[0][4..^1]));
+			foreach (var address in masks.Select(x => ApplyMaskToAddress(baseAddress, x)))
+				memory[address] = int.Parse(pieces[1]);
 		}
 	}
 
-	WriteLine(memory.Sum(x => x.Value));
+	return memory.Sum(x => x.Value);
 }
 
-long ApplyMaskToAddress(string baseAddress, string mask)
-{
-	var str = baseAddress.Zip(mask).Select(x => x.Second switch
+static long ApplyMaskToAddress(string baseAddress, char[] mask)
+	=> FromBinary(baseAddress.Zip(mask).Select(x => x.Second switch
 	{
 		'0' => x.First,
 		'1' => '1',
 		'2' => '0',
 		_ => throw new NotSupportedException(),
-	}).ToArray().AsSpan().ToString();
-	return Convert.ToInt64(str, 2);
-}
+	}).ToArray().AsSpan().ToString());
 
-List<string> GeneratePlainMasks(string mask)
+static List<char[]> GeneratePlainMasks(string mask)
 {
 	var floating = mask.Select((ch, i) => (ch, i)).Where(x => x.ch == 'X').Select(x => x.i).ToArray();
 	var count = floating.Count();
@@ -89,6 +75,9 @@ List<string> GeneratePlainMasks(string mask)
 			var mod = entryIndex % pow;
 			newMask[floatVal] = mod >= pow / 2 ? '1' : '2';
 		}
-		return newMask.AsSpan().ToString();
+		return newMask;
 	}).ToList();
 }
+
+static long FromBinary(string value) => Convert.ToInt64(value, 2);
+static string ToBinary(long value) => Convert.ToString(value, 2).PadLeft(36, '0');
