c72a2eb5c2ce358e17d790b91703f5674363534f
Fred Morel <github@fmorel.net>
2020-12-14 15:18:45 -0500

    Day 14 microoptimizations for fun.

--- 2020/day-14/Program.cs
+++ 2020/day-14/Program.cs
@@ -12,17 +12,15 @@ timer.Restart();
 WriteLine($"{Part2(read)} :: {timer.Elapsed}");
 timer.Stop();
 
-long Part1(string[] read)
+static long Part1(string[] read)
 {
-	long orMask = 0L, andMask = 0L;
+	var masks = new Mask(0, 0);
 	var memory = new Dictionary<long, long>();
 	foreach (var line in read)
 	{
 		if (line.StartsWith("mask", StringComparison.Ordinal))
 		{
-			orMask = 0;
-			andMask = 0;
-			var currentBit = 1L;
+			long orMask = 0, andMask = 0, currentBit = 1;
 			foreach (var bit in line[7..].Reverse())
 			{
 				// If X, set AND mask to 1 in order to get the 0/1 from value
@@ -34,66 +32,65 @@ long Part1(string[] read)
 				// If 0, always set 0 (AND mask applied first covers this)
 				currentBit <<= 1;
 			}
+			masks = new Mask(andMask, orMask);
 		}
 		else
 		{
-			var pieces = line.Split(" = ");
-			memory[long.Parse(pieces[0][4..^1])] = (long.Parse(pieces[1]) & andMask) | orMask;
+			var equalIndex = line.IndexOf('=');
+			memory[long.Parse(line[4..(equalIndex - 2)])] = (long.Parse(line[(equalIndex + 1)..]) & masks.and) | masks.or;
 		}
 	}
 
 	return memory.Sum(x => x.Value);
 }
 
-long Part2(string[] read)
+static long Part2(string[] read)
 {
-	var masks = new List<(long andMask, long orMask)>();
+	var masks = new Mask[0];
 	var memory = new Dictionary<long, long>();
 	foreach (var line in read)
 	{
-		var pieces = line.Split(" = ");
-		if (pieces[0] == "mask")
+		if (line.StartsWith("mask", StringComparison.Ordinal))
 		{
-			masks = GeneratePlainMasks(pieces[1]);
+			var baseMask = line[7..].Reverse().ToArray();
+			masks = new Mask[1 << baseMask.Count(x => x == 'X')];
+			for (var entryIndex = 0; entryIndex < masks.Length; entryIndex++)
+			{
+				long andMask = 0, orMask = 0, currentBit = 1, xFactor = 1;
+				foreach (var bit in baseMask)
+				{
+					// If X, set based on current iteration of float
+					if (bit == 'X')
+					{
+						xFactor *= 2;
+						// half the time, set it to 1, half the time leave it as 0
+						if (entryIndex % xFactor >= xFactor / 2)
+						{
+							orMask |= currentBit;
+						}
+					}
+					// If 1, set OR mask to 1 to make sure we put in 1
+					else if (bit == '1')
+						orMask |= currentBit;
+					// If 0, always set AND mask to keep original value
+					else if (bit == '0')
+						andMask |= currentBit;
+					currentBit <<= 1;
+				}
+				masks[entryIndex] = new Mask(andMask, orMask);
+			};
 		}
 		else
 		{
-			var baseAddress = long.Parse(pieces[0][4..^1]);
-			var value = long.Parse(pieces[1]);
-			foreach (var address in masks.Select(x => (baseAddress & x.andMask) | x.orMask))
-				memory[address] = value;
+			var equalIndex = line.IndexOf('=');
+			var baseAddress = long.Parse(line[4..(equalIndex - 2)]);
+			var value = long.Parse(line[(equalIndex + 1)..]);
+			foreach (var x in masks)
+				memory[(baseAddress & x.and) | x.or] = value;
 		}
 	}
 
 	return memory.Sum(x => x.Value);
 }
 
-static List<(long andMask, long orMask)> GeneratePlainMasks(string baseMask)
-{
-	return Enumerable.Range(1, 1 << baseMask.Count(x => x == 'X')).Select(entryIndex =>
-	{
-		long andMask = 0, orMask = 0, currentBit = 1;
-		long xFactor = 1;
-		foreach (var bit in baseMask.Reverse())
-		{
-			// If X, set based on current iteration of float
-			if (bit == 'X')
-			{
-				xFactor *= 2;
-				// half the time, set it to 1, half the time leave it as 0
-				if ((entryIndex - 1) % xFactor >= xFactor / 2)
-				{
-					orMask |= currentBit;
-				}
-			}
-			// If 1, set OR mask to 1 to make sure we put in 1
-			else if (bit == '1')
-				orMask |= currentBit;
-			// If 0, always set AND mask to keep original value
-			else if (bit == '0')
-				andMask |= currentBit;
-			currentBit <<= 1;
-		}
-		return (andMask, orMask);
-	}).ToList();
-}
+record Mask(long and, long or);
