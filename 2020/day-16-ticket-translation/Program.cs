a178cca175bb8e7a3ccb7b506efeae1308e2a7ee
Fred Morel <github@fmorel.net>
2020-12-17 12:27:38 -0500

    Improve Day 16

--- 2020/day-16-ticket-translation/Program.cs
+++ 2020/day-16-ticket-translation/Program.cs
@@ -12,65 +12,57 @@ var timer = Stopwatch.StartNew();
 var rules = new List<Rule>();
 
 var i = 0;
-while (!string.IsNullOrEmpty(list[i]))
+for (; !string.IsNullOrEmpty(list[i]); i++)
 {
 	var pieces = list[i].Split(": ");
-	var nums = new HashSet<int>();
-	var ranges = pieces[1].Split(" or ").Select(x => x.Split('-').Select(int.Parse).ToList());
-	foreach (var range in ranges)
-		nums.UnionWith(Enumerable.Range(range[0], range[1] - range[0] + 1));
-	rules.Add(new Rule(pieces[0], nums));
-	i++;
+	var ranges = pieces[1].Split(" or ").Select(x => x.Split('-').Select(int.Parse).ToList()).ToList();
+	rules.Add(new Rule(pieces[0], ranges[0][0], ranges[0][1], ranges[1][0], ranges[1][1]));
 }
-i += 2;// skip empty and "your ticket:"
-var myTicket = list[i].Split(',').Select(int.Parse).ToList();
+// skip empty and "your ticket:"
+var myTicket = list[i + 2].Split(',').Select(int.Parse).ToList();
 
-var invalid = new List<int>();
-var valid = new List<List<int>>();
-i += 3;// skip my ticket, empty, and "nearby tickets:"
-foreach (var line in list.Skip(i))
+var allInvalid = new List<int>();
+var tickets = new List<List<int>>();
+// skip my ticket, empty, and "nearby tickets:"
+foreach (var line in list.Skip(i + 5))
 {
 	var nums = line.Split(',').Select(int.Parse).ToList();
-	var isValid = true;
-	foreach (var num in nums)
-	{
-		if (!rules.Any(x => x.allowed.Contains(num)))
-		{
-			invalid.Add(num);
-			isValid = false;
-			break;
-		}
-	}
-	if (isValid)
-		valid.Add(nums);
+	var invalidNums = nums.Where(num => !rules.Any(x => x.Matches(num))).ToList();
+	if (invalidNums.Any())
+		allInvalid.AddRange(invalidNums);
+	else
+		tickets.Add(nums);
 }
 
-WriteLine($"{invalid.Sum()} :: {timer.Elapsed}");
+WriteLine($"{allInvalid.Sum()} :: {timer.Elapsed}");
 timer.Restart();
 
-var fieldsByIndex = new Dictionary<int, string>();
+var matched = new Dictionary<int, string>();
 var unmatched = new List<Rule>(rules);
 while (unmatched.Any())
 {
-	var copy = new List<Rule>(unmatched);
+	var remaining = new List<Rule>(unmatched);
 	unmatched.Clear();
-	foreach (var rule in copy)
+	foreach (var rule in remaining)
 	{
-		var unknown = Enumerable.Range(0, valid[0].Count).Where(x => !fieldsByIndex.ContainsKey(x)).ToList();
-		var matches = unknown.Where(tIndex => valid.All(x => rule.allowed.Contains(x[tIndex]))).ToList();
+		var matches = Enumerable.Range(0, tickets[0].Count)
+			.Where(x => !matched.ContainsKey(x) && tickets.All(ticket => rule.Matches(ticket[x])))
+			.ToList();
 		if (matches.Count() == 1)
-			fieldsByIndex[matches[0]] = rule.name;
+			matched[matches[0]] = rule.name;
 		else
 			unmatched.Add(rule);
 	}
 }
 
-var part2 = fieldsByIndex
+var part2 = matched
 	.Where(x => x.Value.StartsWith("departure ", StringComparison.Ordinal))
-	.Select(x => myTicket[x.Key])
-	.Aggregate(1L, (acc, value) => acc * value);
+	.Aggregate(1L, (acc, x) => acc * myTicket[x.Key]);
 
 WriteLine($"{part2} :: {timer.Elapsed}");
 timer.Stop();
 
-record Rule(string name, HashSet<int> allowed);
+record Rule(string name, int aMin, int aMax, int bMin, int bMax)
+{
+	public bool Matches(int num) => (num >= aMin && num <= aMax) || (num >= bMin && num <= bMax);
+}
