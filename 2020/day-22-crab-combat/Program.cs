40c75a1cff9c82028b08a52577ee9c20c7d91484
Fred Morel <github@fmorel.net>
2020-12-22 00:51:54 -0500

    Refactor Day 22, part 2 to make it faster

--- 2020/day-22-crab-combat/Program.cs
+++ 2020/day-22-crab-combat/Program.cs
@@ -17,100 +17,73 @@ timer.Stop();
 
 long Part1()
 {
-	var (p1Deck, p2Deck) = ResetDecks();
+	var (p1Deck, p2Deck) = GetOriginalDecks();
 	while (p1Deck.Any() && p2Deck.Any())
 	{
 		int p1 = p1Deck.Dequeue(), p2 = p2Deck.Dequeue();
-		if (p1 > p2)
-		{
-			p1Deck.Enqueue(p1);
-			p1Deck.Enqueue(p2);
-		}
-		else if (p1 < p2)
-		{
-			p2Deck.Enqueue(p2);
-			p2Deck.Enqueue(p1);
-		}
-		else
-		{
-			p1Deck.Enqueue(p1);
-			p2Deck.Enqueue(p2);
-		}
+		AddCardsToWinner(p1Deck, p2Deck, p1, p2, GetWinner(p1, p2));
 	}
-	var size = p1Deck.Count + p2Deck.Count;
-	return p1Deck.Concat(p2Deck).Select((card, i) => card * (size - i)).Sum();
+	return GetScore(p1Deck, p2Deck);
 }
 
 long Part2()
 {
-	var (p1Deck, p2Deck) = ResetDecks();
-	var result = PlayRecursive(p1Deck, p2Deck);
-
-	var size = result.p1Deck.Count() + result.p2Deck.Count();
-	return result.p1Deck.Concat(result.p2Deck).Select((card, i) => card * (size - i)).Sum();
+	var (p1Deck, p2Deck) = GetOriginalDecks();
+	(p1Deck, p2Deck) = PlayRecursive(p1Deck, p2Deck);
+	return GetScore(p1Deck, p2Deck);
 }
+
+long GetScore(IEnumerable<int> p1, IEnumerable<int> p2)
+	=> p1.Concat(p2).Reverse().Select((x, i) => x * (i + 1)).Sum();
 
 (Queue<int> p1Deck, Queue<int> p2Deck) PlayRecursive(Queue<int> p1Deck, Queue<int> p2Deck)
 {
-	var startingDecks = new List<State>();
+	var states = new List<Memory>();
 
 	while (p1Deck.Any() && p2Deck.Any())
 	{
-		if (startingDecks.Any(x => x.p1.SequenceEqual(p1Deck) && x.p2.SequenceEqual(p2Deck)))
-		{
-			p2Deck.Clear();
-			break;
-		}
-		var p1deckImmutable = ImmutableQueue.CreateRange(p1Deck);
-		var p2deckImmutable = ImmutableQueue.CreateRange(p2Deck);
-		startingDecks.Add(new(p1deckImmutable, p2deckImmutable));
+		if (states.Any(x => x.p1.SequenceEqual(p1Deck) && x.p2.SequenceEqual(p2Deck)))
+			return (p1Deck, new Queue<int>());
+
+		states.Add(new(p1Deck.ToArray(), p2Deck.ToArray()));
 
 		int p1 = p1Deck.Dequeue(), p2 = p2Deck.Dequeue();
 		if (p1 <= p1Deck.Count && p2 <= p2Deck.Count)
 		{
-			var result = PlayRecursive(new(p1Deck.Take(p1)), new(p2Deck.Take(p2)));
-			var p1Size = result.p1Deck.Count;
-			var p2Size = result.p2Deck.Count;
-			if (p1Size > p2Size)
-			{
-				p1Deck.Enqueue(p1);
-				p1Deck.Enqueue(p2);
-			}
-			else if (p2Size > p1Size)
-			{
-				p2Deck.Enqueue(p2);
-				p2Deck.Enqueue(p1);
-			}
-			else
-			{
-				p1Deck.Enqueue(p1);
-				p2Deck.Enqueue(p2);
-			}
+			var (p1Sub, p2Sub) = PlayRecursive(new(p1Deck.Take(p1)), new(p2Deck.Take(p2)));
+			AddCardsToWinner(p1Deck, p2Deck, p1, p2, GetWinner(p1Sub.Count, p2Sub.Count));
 		}
 		else
 		{
-			if (p1 > p2)
-			{
-				p1Deck.Enqueue(p1);
-				p1Deck.Enqueue(p2);
-			}
-			else if (p1 < p2)
-			{
-				p2Deck.Enqueue(p2);
-				p2Deck.Enqueue(p1);
-			}
-			else
-			{
-				p1Deck.Enqueue(p1);
-				p2Deck.Enqueue(p2);
-			}
+			AddCardsToWinner(p1Deck, p2Deck, p1, p2, GetWinner(p1, p2));
 		}
 	}
 
 	return (p1Deck, p2Deck);
 }
 
-(Queue<int> p1Deck, Queue<int> p2Deck) ResetDecks()
+int GetWinner(int p1Value, int p2Value) => p1Value > p2Value ? 1 : p2Value > p1Value ? 2 : 0;
+
+void AddCardsToWinner(Queue<int> p1Deck, Queue<int> p2Deck, int p1Card, int p2Card, int winner)
+{
+	switch (winner)
+	{
+		case 1:
+			p1Deck.Enqueue(p1Card);
+			p1Deck.Enqueue(p2Card);
+			break;
+		case 2:
+			p2Deck.Enqueue(p2Card);
+			p2Deck.Enqueue(p1Card);
+			break;
+		default:
+			p1Deck.Enqueue(p1Card);
+			p2Deck.Enqueue(p2Card);
+			break;
+	}
+}
+
+(Queue<int> p1Deck, Queue<int> p2Deck) GetOriginalDecks()
 {
 	Queue<int> p1deck = new(), p2deck = new();
 	var i = 1;
@@ -123,4 +96,4 @@ long Part2()
 	return (p1deck, p2deck);
 }
 
-record State(ImmutableQueue<int> p1, ImmutableQueue<int> p2);
+record Memory(int[] p1, int[] p2);
