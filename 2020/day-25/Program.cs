571d2ae62f083531ff2b25664d8bfdd10a4f0961
Fred Morel <github@fmorel.net>
2020-12-23 23:54:35 -0500

    Set up last 2 days

---
+++ 2020/day-25/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+var list = System.IO.File.ReadAllLines("input.txt");
+
+
+
+var timer = Stopwatch.StartNew();
+WriteLine($"{Part1()} :: {timer.Elapsed}");
+timer.Restart();
+WriteLine($"{Part2()} :: {timer.Elapsed}");
+timer.Stop();
+
+long Part1()
+{
+	return 0;
+}
+
+long Part2()
+{
+	return 0;
+}
