290535be671f5d902f45c23c8f0b0586f3f0c6e7
Fred Morel <github@fmorel.net>
2020-12-23 23:53:39 -0500

    Rename Day 23 folder

--- 2020/day-23-crab-cups/Program.cs
+++ 2020/day-23-crab-cups/Program.cs
@@ -9,7 +9,7 @@ using static System.Console;
 
 var list = System.IO.File.ReadAllLines("input.txt");
 
-var startingCups = list[0].Select(ch => int.Parse(ch.ToString())).ToList();
+var startingCups = list[0].Select(ch => int.Parse(ch.ToString())).ToArray();
 
 var timer = Stopwatch.StartNew();
 WriteLine($"{Part1()} :: {timer.Elapsed}");
@@ -17,17 +17,14 @@ timer.Restart();
 WriteLine($"{Part2()} :: {timer.Elapsed}");
 timer.Stop();
 
-string Part1()
+long Part1()
 {
 	var pointers = RunSimulation(startingCups, 100, 9);
-	var result = new StringBuilder();
 	var current = pointers[1];
+	var result = new StringBuilder().Append(current);
 	while (current != 1)
-	{
-		result.Append(current);
-		current = pointers[current];
-	}
-	return result.ToString();
+		result.Append(current = pointers[current]);
+	return long.Parse(result.Remove(8, 1).ToString());
 }
 
 long Part2()
@@ -36,55 +33,48 @@ long Part2()
 	return (long)pointers[1] * (long)pointers[pointers[1]];
 }
 
-int[] RunSimulation(List<int> startingCups, int iterations, int max)
+int[] RunSimulation(int[] startingCups, int iterations, int max)
 {
-	WriteLine($"{timer.Elapsed} :: create lookup");
-
+	var removed = new int[3];
 	var pointers = new int[max + 1];
 
 	// initialize 10-max in numerical order
 	for (var i = 10; i <= max; i++)
 		pointers[i] = i + 1;
 	// initialize 1-9 in input order
-	for (var i = 0; i < startingCups.Count - 1; i++)
+	for (var i = 0; i < startingCups.Length - 1; i++)
 		pointers[startingCups[i]] = startingCups[i + 1];
 
 	var current = startingCups[0];
 
 	// If only 9 cups, make last input point to first input
-	if (max == startingCups.Count)
-		pointers[startingCups.Last()] = current;
+	if (max == startingCups.Length)
+		pointers[startingCups[^1]] = current;
 	// Otherwise last input => 10 && max >= first input
 	else
 	{
-		pointers[startingCups.Last()] = 10;
+		pointers[startingCups[^1]] = 10;
 		pointers[max] = current;
 	}
 
-	var removed = new int[3];
-
-	WriteLine($"{timer.Elapsed} :: iterate");
 	for (var i = 0; i < iterations; i++)
 	{
+		// Store removed cups
 		removed[0] = pointers[current];
 		removed[1] = pointers[removed[0]];
 		removed[2] = pointers[removed[1]];
-		pointers[current] = pointers[removed[2]];
 
+		// Figure out where they go
 		var destination = current - 1;
 		while (destination == 0 || removed.Contains(destination))
-		{
-			if (destination == 0)
-				destination = max;
-			else
-				destination--;
-		}
+			destination = destination == 0 ? max : destination - 1;
+
+		// Update cup locations
+		pointers[current] = pointers[removed[2]];
 		pointers[removed[2]] = pointers[destination];
 		pointers[destination] = removed[0];
-
 		current = pointers[current];
 	}
 
-	WriteLine($"{timer.Elapsed} :: done");
 	return pointers;
 }
