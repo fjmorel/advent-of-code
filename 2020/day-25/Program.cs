256189755e66d9b26e05337945021d697130f075
Fred Morel <github@fmorel.net>
2021-11-22 09:32:22 -0500

    forgot to commit Day 25 of 2020

--- 2020/day-25/Program.cs
+++ 2020/day-25/Program.cs
@@ -9,20 +9,43 @@ using static System.Console;
 
 var list = System.IO.File.ReadAllLines("input.txt");
 
+var publicKeys = list.Select(long.Parse).ToList();
+var cardPublic = publicKeys[0];
+var doorPublic = publicKeys[1];
 
 
 var timer = Stopwatch.StartNew();
 WriteLine($"{Part1()} :: {timer.Elapsed}");
-timer.Restart();
-WriteLine($"{Part2()} :: {timer.Elapsed}");
 timer.Stop();
 
 long Part1()
 {
-	return 0;
+	var cardLoop = GetLoopSizeFromPublickKey(cardPublic);
+	var doorLoop = GetLoopSizeFromPublickKey(doorPublic);
+
+	var encryption1 = PublicToEncryptionKey(doorPublic, cardLoop);
+	var encryption2 = PublicToEncryptionKey(cardPublic, doorLoop);
+	Debug.Assert(encryption1 == encryption2);
+	return encryption1;
 }
 
-long Part2()
+long PublicToEncryptionKey(long publicKey, long loopSize)
 {
-	return 0;
+	long subject = publicKey, current = 1;
+	for (var i = 1; i <= loopSize; i++)
+	{
+		current = (current * subject) % 20201227;
+	}
+	return current;
+}
+
+long GetLoopSizeFromPublickKey(long publicKey)
+{
+	int subject = 7, loopSize = 0, current = 1;
+	while (current != publicKey)
+	{
+		loopSize++;
+		current = (current * subject) % 20201227;
+	}
+	return loopSize;
 }
