a223f567127fb027e2122cee7cf57e4d349cf98c
Fred Morel <github@fmorel.net>
2021-11-26 12:06:27 -0500

    Day 10, part 2 finally

--- 2019/10/Program.cs
+++ 2019/10/Program.cs
@@ -1,10 +1,16 @@
-﻿var list = File.ReadAllLines("input.txt");
+﻿var timer = Stopwatch.StartNew();
+
+var list = File.ReadAllLines("input.txt");
 // list = File.ReadAllLines("example.txt");
 var grid = list.Select(line => line.Select(x => x == '#').ToArray()).ToArray();
 var height = grid.Length;
 var width = grid[0].Length;
+var slopes = GetSlopes(width, height).ToHashSet();
+var asteroids = GetAsteroids().ToHashSet();
+var visiblePerAsteroid = asteroids.ToDictionary(x => x, x => x.GetVisibleAsteroids(asteroids, slopes, width, height));
+var best = visiblePerAsteroid.MaxBy(x => x.Value.Count);
 
-var timer = Stopwatch.StartNew();
+Console.WriteLine($"_ :: {timer.Elapsed}");// setup time
 Console.WriteLine($"{Part1()} :: {timer.Elapsed}");
 timer.Restart();
 Console.WriteLine($"{Part2()} :: {timer.Elapsed}");
@@ -12,36 +18,27 @@ timer.Stop();
 
 long Part1()
 {
-	var slopes = GetSlopes().ToHashSet();
-	var asteroids = GetAsteroids().ToHashSet();
-	var visibleAtEachAsteroid = new Dictionary<Point, HashSet<Point>>();
-
-	foreach (var station in asteroids)
-	{
-		var detected = new HashSet<Point>();
-		foreach (var slope in slopes)
-		{
-			var pt = station + slope;
-			while (pt.WithinGrid(width, height))
-			{
-				if (asteroids.Contains(pt))
-				{
-					detected.Add(pt);
-					break;
-				}
-				pt += slope;
-			}
-		}
-		visibleAtEachAsteroid[station] = detected;
-	}
-
-	var best = visibleAtEachAsteroid.MaxBy(x => x.Value.Count);
 	return best.Value.Count;
 }
 
 long Part2()
 {
-	return -1;
+	// Identify station and initial visible asteroids
+	var station = best.Key;
+	var visible = best.Value;
+
+	var leftover = new HashSet<Point>(asteroids);
+	var destroyed = new List<Point>();
+	while (leftover.Count > 1 && destroyed.Count < 200)
+	{
+		// Destroy the visible asteroids, then find new ones to destroy
+		destroyed.AddRange(visible.OrderBy(x => x.GetAngleFrom(station)));
+		leftover.ExceptWith(visible);
+		visible = station.GetVisibleAsteroids(leftover, slopes, width, height);
+	}
+
+	var theOne = destroyed[199];
+	return theOne.x * 100 + theOne.y;
 }
 
 IEnumerable<Point> GetAsteroids()
@@ -57,7 +54,7 @@ IEnumerable<Point> GetAsteroids()
 	}
 }
 
-IEnumerable<Point> GetSlopes()
+IEnumerable<Point> GetSlopes(int width, int height)
 {
 	yield return new(0, 1);
 	yield return new(0, -1);
@@ -105,6 +102,78 @@ IEnumerable<Point> GetSlopes()
 
 readonly record struct Point(int x, int y)
 {
+	public static Point operator +(Point a) => a;
+	public static Point operator -(Point a) => new(-a.x, -a.y);
 	public static Point operator +(Point a, Point b) => new(a.x + b.x, a.y + b.y);
-	public bool WithinGrid(int width, int height) => this.x >= 0 && this.x < width && this.y >= 0 && this.y < height;
+	public static Point operator -(Point a, Point b) => a + (-b);
+	public double GetAngleFrom(Point origin)
+	{
+		var fromOrigin = this - origin;
+		fromOrigin = fromOrigin with { y = -fromOrigin.y };// invert y to fix above/below
+		var hypotenuse = Math.Sqrt(Math.Pow(fromOrigin.x, 2) + Math.Pow(fromOrigin.y, 2));
+
+		double theta = 0;
+		if (fromOrigin.y == 0)
+		{
+			var ah = fromOrigin.x / hypotenuse;
+			theta = Math.Acos(ah);
+		}
+		else
+		{
+			var oh = fromOrigin.y / hypotenuse;
+			theta = Math.Asin(oh);
+
+			// Make sure to correctly convert -x to left side of circle
+			if (fromOrigin.x < 0)
+			{
+				// top-right to top-left
+				if (theta > 0)
+					theta = Math.PI - theta;
+				// bottom-right to bottom-left
+				else if (theta < 0)
+					theta = -Math.PI - theta;
+			}
+		}
+		var degrees = (180 / Math.PI) * theta;
+
+		// turn normal unit circle into 0 at top going clockwise
+		var adj = degrees switch
+		{
+			90 => 0,
+			0 => 90,
+			-90 => 180,
+			-180 => 270,
+			180 => 270,
+
+			> 0 and < 90 => (90 - degrees),// reverse it
+			> 90 and < 180 => (180 - degrees + 270),// reverse it and bump it up to last quadrant
+
+			> -180 and < -90 => (-degrees + 90),// right order, make it positive and bump up to 3rd quadrant
+			> -90 and < 0 => (-degrees + 90),// right order, make it positive and bump up to 2nd quadrant
+
+			_ => throw new NotSupportedException("Unexpected degree value: " + degrees),
+		};
+
+		return adj;
+	}
+
+	public HashSet<Point> GetVisibleAsteroids(HashSet<Point> asteroids, HashSet<Point> slopes, int width, int height)
+	{
+		var detected = new HashSet<Point>();
+		// Explore along each possible line to find an asteroid within the grid
+		foreach (var slope in slopes)
+		{
+			var pt = this + slope;
+			while (pt.x >= 0 && pt.x < width && pt.y >= 0 && pt.y < height)
+			{
+				if (asteroids.Contains(pt))
+				{
+					detected.Add(pt);
+					break;
+				}
+				pt += slope;
+			}
+		}
+		return detected;
+	}
 }
