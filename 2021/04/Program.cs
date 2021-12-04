161e0a3a9579623fcb0df72400fbf776ecc93bb4
Fred Morel <github@fmorel.net>
2021-12-04 17:43:27 -0500

    Slower, but much more concise and hard to read

--- 2021/04/Program.cs
+++ 2021/04/Program.cs
@@ -2,7 +2,11 @@
 var lines = File.ReadAllLines("input.txt");
 // lines = File.ReadAllLines("example.txt");
 var numbers = lines[0].Split(',').Select(x => int.Parse(x)).ToArray();
-var cards = lines.Skip(1).Chunk(6).Select(sixLines => sixLines.Skip(1).SelectMany(line => line.Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries).Select(x => int.Parse(x))).ToArray()).ToList();
+var lookup = numbers.Select((num, i) => (num, i)).ToDictionary(x => x.num, x => x.i);
+var cards = lines
+	.Skip(1).Chunk(6)
+	.Select(sixLines => sixLines.SelectMany(line => Regex.Matches(line, "([0-9]+)").Select(x => int.Parse(x.ValueSpan))).ToArray())
+	.ToList();
 
 Console.WriteLine($"_ :: {timer.Elapsed}");// setup time
 Console.WriteLine($"{Part1()} :: {timer.Elapsed}");
@@ -10,87 +14,20 @@ timer.Restart();
 Console.WriteLine($"{Part2()} :: {timer.Elapsed}");
 timer.Stop();
 
-long Part1()
-{
-	var lowest = numbers.Length + 1;
-	int[]? winningCard = null;
-	foreach (var card in cards)
-	{
-		var count = CountNeededToWin(card);
-		if (count < lowest)
-		{
-			lowest = count;
-			winningCard = card;
-		}
-	}
+long Part1() => GetScore(cards.MinBy(CountNeededToWin)!);
 
-	return GetScore(winningCard, lowest);
+long Part2() => GetScore(cards.MaxBy(CountNeededToWin)!);
+
+long GetScore(int[] card)
+{
+	var needed = CountNeededToWin(card);
+	return card.Except(numbers[0..needed]).Sum() * numbers[needed - 1];
 }
 
-long Part2()
-{
-	var highest = 0;
-	int[]? winningCard = null;
-	foreach (var card in cards)
-	{
-		var count = CountNeededToWin(card);
-		if (count > highest)
-		{
-			highest = count;
-			winningCard = card;
-		}
-	}
+int CountNeededToWin(int[] card) => Enumerable.Range(1, numbers.Length).FirstOrDefault(i => HasWinningLine(card, i), -1);
 
-	return GetScore(winningCard, highest);
-}
-
-long GetScore(int[] card, int needed)
-{
-	var list = card.ToList();
-	var marked = new List<int>();
-	for (var i = 0; i < needed; i++)
-	{
-		if (list.Remove(numbers[i]))
-			marked.Add(numbers[i]);
-	}
-	return list.Sum() * numbers[needed - 1];
-}
-
-int CountNeededToWin(int[] card)
-{
-	var currentSet = new HashSet<int>(numbers.Length);
-	for (var i = 0; i < numbers.Length; i++)
-	{
-		currentSet.Add(numbers[i]);
-		if (HasWinningLine(card, currentSet))
-			return i + 1;
-	}
-	return numbers.Length + 1;
-}
-
-bool HasWinningLine(int[] card, HashSet<int> numbers)
-{
-	for (var i = 0; i < 5; i++)
-	{
-		// Each horizontal line
-		for (var j = 0; j < 5; j++)
-		{
-			var index = (i * 5) + j;
-			if (!numbers.Contains(card[index]))
-				break;
-			if (j == 4)
-				return true;
-		}
-		// Each vertical line
-		for (var j = 0; j < 5; j++)
-		{
-			var index = (j * 5) + i;
-			if (!numbers.Contains(card[index]))
-				break;
-			if (j == 4)
-				return true;
-		}
-	}
-
-	return false;
-}
+bool HasWinningLine(int[] card, int numDrawn) =>
+	Enumerable.Range(0, 5).Any(i =>
+		Enumerable.Range(0, 5).All(j => lookup[card[(i * 5) + j]] < numDrawn)
+		|| Enumerable.Range(0, 5).All(j => lookup[card[(j * 5) + i]] < numDrawn)
+	);
