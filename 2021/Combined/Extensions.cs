f4eb1e7cce0d7696dc5843cf6901d6792f2fe83e
Fred Morel <github@fmorel.net>
2021-12-11 00:51:34 -0500

    Update Extensions.cs

--- 2021/Combined/Extensions.cs
+++ 2021/Combined/Extensions.cs
@@ -3,4 +3,5 @@
     internal static List<long> ParseLongs(this string[] lines) => lines.Select(x => long.Parse(x)).ToList();
     internal static List<int> ParseInts(this string[] lines) => lines.Select(x => int.Parse(x)).ToList();
     internal static int[] ParseCsvInts(this string line) => line.Split(',').Select(int.Parse).ToArray();
+    internal static int[] ParseDigits(this string line) => line.Select(c => c - 48).ToArray();
 }
