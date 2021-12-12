dd6f1b551f1a0e804b2cdc3855eac789a5a5a726
Fred Morel <github@fmorel.net>
2021-12-11 23:58:16 -0500

    Prep

---
+++ 2021/Puzzles2021/Solutions/Solution23.cs
@@ -0,0 +1,20 @@
+namespace Puzzles2021.Solutions;
+
+public class Solution23 : ISolution
+{
+
+	public Solution23(string[] lines)
+	{
+	}
+
+	public async ValueTask<long> GetPart1()
+	{
+		return 0;
+	}
+
+	public async ValueTask<long> GetPart2()
+	{
+		return 0;
+	}
+
+}
--- 2021/Tests2021/WorkInProgressTests.cs
+++ 2021/Tests2021/WorkInProgressTests.cs
@@ -3,28 +3,21 @@
 public class WorkInProgressTests
 {
     private const string _day = "12";
+    private const long _example1 = 00000;
+    private const long _example2 = 00000;
+
+    private const long _input1 = 00000;
+    private const long _input2 = 00000;
 
     [Fact]
-    public async Task DailyExample1()
-    {
-        await Helpers.RunPart1(_day, "examples", 0);
-    }
+    public Task DailyExample1() => Helpers.RunPart1(_day, "examples", _example1);
 
     [Fact]
-    public async Task DailyExample2()
-    {
-        await Helpers.RunPart2(_day, "examples", 0);
-    }
+    public Task DailyExample2() => Helpers.RunPart2(_day, "examples", _example2);
 
     [Fact]
-    public async Task DailyInput1()
-    {
-        await Helpers.RunPart1(_day, "inputs", 0);
-    }
+    public Task DailyInput1() => Helpers.RunPart1(_day, "inputs", _input1);
 
     [Fact]
-    public async Task DailyInput2()
-    {
-        await Helpers.RunPart2(_day, "inputs", 0);
-    }
+    public Task DailyInput2() => Helpers.RunPart2(_day, "inputs", _input2);
 }
