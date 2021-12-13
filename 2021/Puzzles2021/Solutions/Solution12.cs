6742d2d04ea499c59cfde089bbd34a2b151f9731
Fred Morel <github@fmorel.net>
2021-12-12 22:40:19 -0500

    Simplify day 12

--- 2021/Puzzles2021/Solutions/Solution12.cs
+++ 2021/Puzzles2021/Solutions/Solution12.cs
@@ -9,21 +9,15 @@ public class Solution12 : ISolution
         _connections = new();
         foreach (var line in lines)
         {
-            var split = line.Split('-');
-            if (!_connections.ContainsKey(split[0]))
-                _connections[split[0]] = new();
+            var pair = line.Split('-');
+            _connections.TryAdd(pair[0], new());
+            _connections.TryAdd(pair[1], new());
             // don't let  anyone go back to the start
-            if (split[1] != "start")
-                _connections[split[0]].Add(split[1]);
-
-            if (!_connections.ContainsKey(split[1]))
-                _connections[split[1]] = new();
-            // don't let  anyone go back to the start
-            if (split[0] != "start")
-                _connections[split[1]].Add(split[0]);
+            if (pair[1] != "start")
+                _connections[pair[0]].Add(pair[1]);
+            if (pair[0] != "start")
+                _connections[pair[1]].Add(pair[0]);
         }
-
-        _connections["end"] = new();
     }
 
     public async ValueTask<long> GetPart1()
@@ -34,12 +28,12 @@ public class Solution12 : ISolution
         while (queue.Any())
         {
             var state = queue.Dequeue();
+            state.Visited.Add(state.Current);
             var newRoutes = _connections[state.Current]
                 .Where(cave => !state.Visited.Contains(cave) || cave.ToUpper() == cave)
                 .Select(cave => state with { Current = cave, Visited = new(state.Visited) });
             foreach (var route in newRoutes)
             {
-                route.Visited.Add(route.Current);
                 if (route.Current == "end")
                     finished.Add(route);
                 else
@@ -58,20 +52,20 @@ public class Solution12 : ISolution
         while (queue.Any())
         {
             var state = queue.Dequeue();
-            foreach (var path in FindPaths2(state))
+            state.Visited.Add(state.Current);
+            foreach (var route in FindRoutes2(state))
             {
-                path.Visited.Add(path.Current);
-                if (path.Current == "end")
-                    finished.Add(path);
+                if (route.Current == "end")
+                    finished.Add(route);
                 else
-                    queue.Enqueue(path);
+                    queue.Enqueue(route);
             }
         }
 
         return finished.Count;
     }
 
-    public IEnumerable<Route> FindPaths2(Route state)
+    public IEnumerable<Route> FindRoutes2(Route state)
     {
         foreach (var next in _connections[state.Current])
         {
@@ -91,7 +85,7 @@ public class Solution12 : ISolution
     public record Route
     {
         public string Current { get; init; } = "start";
-        public HashSet<string> Visited { get; init; } = new() { "start" };
+        public HashSet<string> Visited { get; init; } = new();
         public bool Revisited { get; set; }
     }
 }
