8e07811836572623f0b557e27149c0e779cba0cf
Fred Morel <github@fmorel.net>
2021-12-14 14:38:04 -0500

    Extreme LINQ. Never do this at work.

--- 2021/Puzzles2021/Solutions/Solution14.cs
+++ 2021/Puzzles2021/Solutions/Solution14.cs
@@ -2,15 +2,13 @@ namespace Puzzles2021.Solutions;
 
 public class Solution14 : ISolution
 {
-    private readonly HashSet<char> _letters;
-    private readonly Dictionary<(char, char), char> _rules;
+    private readonly Dictionary<(char before, char after), char> _rules;
     private readonly string _template;
 
     public Solution14(string[] lines)
     {
         _template = lines[0];
         _rules = lines.Skip(2).ToDictionary(x => (x[0], x[1]), x => x[^1]);
-        _letters = _rules.SelectMany(x => new char[] { x.Key.Item1, x.Key.Item2, x.Value }).ToHashSet();
     }
 
     public async ValueTask<long> GetPart1() => Run(10);
@@ -19,45 +17,27 @@ public class Solution14 : ISolution
 
     private long Run(int iterations)
     {
-        var start = GetEmpty();
+        var start = _rules.ToDictionary(x => x.Key, _ => 0L);
         for (var i = 1; i < _template.Length; i++)
             start[(_template[i - 1], _template[i])]++;
 
-        var final = Enumerable.Range(1, iterations).Aggregate(start, Insert);
-        var totals = GetCounts(final);
-        return totals.Max() - totals.Min();
-    }
+        var counts = Enumerable
+            .Range(1, iterations)
+            .Aggregate(start, (previous, _) =>
+                previous.Aggregate(_rules.ToDictionary(x => x.Key, _ => 0L), (next, kv) =>
+                {
+                    var ((before, after), count) = kv;
+                    var middle = _rules[(before, after)];
+                    next[(before, middle)] += count;
+                    next[(middle, after)] += count;
+                    return next;
+                }))
+            .SelectMany(kv => new (char letter, long count)[] { (kv.Key.before, kv.Value), (kv.Key.after, kv.Value) })
+            .GroupBy(x => x.letter).ToDictionary(x => x.Key, x => x.Sum(y => y.count) / 2);
 
-    private Dictionary<(char, char), long> GetEmpty() => _rules.ToDictionary(x => x.Key, _ => 0L);
+        counts[_template[0]] += 1;
+        counts[_template[^1]] += 1;
 
-    private IReadOnlyCollection<long> GetCounts(Dictionary<(char, char), long> pairs)
-    {
-        var result = _letters.ToDictionary(x => x, _ => 0L);
-
-        foreach (var ((before, after), value) in pairs)
-        {
-            result[before] += value;
-            result[after] += value;
-        }
-
-        foreach (var letter in _letters)
-            result[letter] /= 2;
-        result[_template[0]] += 1;
-        result[_template[^1]] += 1;
-
-        return result.Values;
-    }
-
-    private Dictionary<(char, char), long> Insert(Dictionary<(char, char), long> previous, int _)
-    {
-        var next = GetEmpty();
-        foreach (var ((before, after), value) in previous)
-        {
-            var middle = _rules[(before, after)];
-            next[(before, middle)] += value;
-            next[(middle, after)] += value;
-        }
-
-        return next;
+        return counts.Values.Max() - counts.Values.Min();
     }
 }
