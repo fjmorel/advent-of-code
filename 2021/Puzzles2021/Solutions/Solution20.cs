071f5054f8501bffa51f0e0f8f83afd78b273ca1
Fred Morel <github@fmorel.net>
2021-12-20 10:15:44 -0500

    Day 20

--- 2021/Puzzles2021/Solutions/Solution20.cs
+++ 2021/Puzzles2021/Solutions/Solution20.cs
@@ -2,19 +2,60 @@ namespace Puzzles2021.Solutions;
 
 public class Solution20 : ISolution
 {
+    private readonly Dictionary<Point, bool> _image;
+    private readonly bool[] _algorithm;
 
-	public Solution20(string[] lines)
-	{
-	}
+    public Solution20(string[] lines)
+    {
+        _algorithm = lines[0].Select(x => x == '#').ToArray();
+        _image = lines.Skip(2).SelectMany((line, y) => line.Select((ch, x) => (ch, x, y))).ToDictionary(tuple => new Point(tuple.x, tuple.y), tuple => tuple.ch == '#');
+    }
 
-	public async ValueTask<long> GetPart1()
-	{
-		return 0;
-	}
+    public async ValueTask<long> GetPart1() => GetLitPixelsAfter(2);
 
-	public async ValueTask<long> GetPart2()
-	{
-		return 0;
-	}
+    public async ValueTask<long> GetPart2() => GetLitPixelsAfter(50);
 
+    public long GetLitPixelsAfter(int iterations)
+        => Enumerable.Range(1, iterations)
+            .Aggregate(new Data(false, _image), (data, _) => Enhance(data))
+            .Defined.LongCount(x => x.Value);
+
+    public Data Enhance(Data data)
+    {
+        var (infiniteLit, original) = data;
+        var newImage = new Dictionary<Point, bool>();
+        const int expansion = 2;
+
+        var minX = original.Min(pair => pair.Key.x);
+        var minY = original.Min(pair => pair.Key.y);
+        var maxX = original.Max(pair => pair.Key.x);
+        var maxY = original.Max(pair => pair.Key.y);
+
+        for (var y = minY - expansion; y < maxY + expansion; y++)
+        {
+            for (var x = minX - expansion; x < maxX + expansion; x++)
+            {
+                var pt = new Point(x, y);
+                var pixels = new bool[]
+                {
+                    original.GetValueOrDefault(pt + new Point(-1, -1), infiniteLit),
+                    original.GetValueOrDefault(pt + new Point(0, -1), infiniteLit),
+                    original.GetValueOrDefault(pt + new Point(1, -1), infiniteLit),
+                    original.GetValueOrDefault(pt + new Point(-1, 0), infiniteLit),
+                    original.GetValueOrDefault(pt + new Point(0, 0), infiniteLit),
+                    original.GetValueOrDefault(pt + new Point(1, 0), infiniteLit),
+                    original.GetValueOrDefault(pt + new Point(-1, 1), infiniteLit),
+                    original.GetValueOrDefault(pt + new Point(0, 1), infiniteLit),
+                    original.GetValueOrDefault(pt + new Point(1, 1), infiniteLit),
+                };
+
+                var index = Convert.ToInt32(string.Join("", pixels.Select(p => p ? '1' : '0')), 2);
+                newImage[pt] = _algorithm[index];
+            }
+        }
+
+        return new(_algorithm[infiniteLit ? ^1 : 0], newImage);
+    }
+
+    public record Data(bool LitInfinite, Dictionary<Point, bool> Defined);
 }
--- 2021/Tests2021/WorkInProgressTests.cs
+++ 2021/Tests2021/WorkInProgressTests.cs
@@ -4,7 +4,7 @@ public class WorkInProgressTests
 {
     private static readonly SolutionTester _runner = new(typeof(Solution01).Assembly);
 
-    private const string _day = "20";
+    private const string _day = "21";
     private const long _example1 = 0000;
     private const long _example2 = 0000;
 
