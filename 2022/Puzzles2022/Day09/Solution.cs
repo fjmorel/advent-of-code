dead636e044a6bdfcd28ea09e3f2999c0b8db408
Fred Morel <github@fmorel.net>
2022-12-09 00:50:09 -0500

    Day 9

--- 2022/Puzzles2022/Day09/Solution.cs
+++ 2022/Puzzles2022/Day09/Solution.cs
@@ -1,19 +1,65 @@
 namespace Puzzles2022.Day09;
 
-public record Solution(string[] _lines) : ISolution<Solution>
+public record Solution(List<(char direction, int magnitude)> _lines) : ISolution<Solution>
 {
-    public static Solution Init(string[] lines)
+    public static Solution Init(string[] lines) => new(lines.Select(x => (x[0], int.Parse(x.AsSpan()[2..]))).ToList());
+
+    public async ValueTask<long> GetPart1() => MoveKnots(2);
+
+    public async ValueTask<long> GetPart2() => MoveKnots(10);
+
+    public long MoveKnots(int numOfKnots)
     {
-        return new Solution(lines);
+        var knots = new Point[numOfKnots];
+        var visited = new HashSet<Point>();
+        foreach (var (direction, magnitude) in _lines)
+        {
+            for (var i = 1; i <= magnitude; i++)
+            {
+                knots[0] = Move(knots[0], direction);
+                for (var k = 1; k < knots.Length; k++)
+                {
+                    knots[k] = Catchup(knots[k - 1], knots[k]);
+                    if (k == numOfKnots - 1)
+                        visited.Add(knots[k]);
+                }
+            }
+        }
+
+        return visited.Count;
     }
 
-    public async ValueTask<long> GetPart1()
+    public static Point Catchup(Point head, Point tail)
     {
-        return 0;
+        if (head == tail || head.GetAllAdjacent().Contains(tail))
+            return tail;
+
+        // Move orthogonally to keep up
+        if (tail == head with { x = head.x - 2 })
+            return tail with { x = head.x - 1 };
+
+        if (tail == head with { x = head.x + 2 })
+            return tail with { x = head.x + 1 };
+
+        if (tail == head with { y = head.y - 2 })
+            return tail with { y = head.y - 1 };
+
+        if (tail == head with { y = head.y + 2 })
+            return tail with { y = head.y + 1 };
+
+        // Move diagonally to keep up
+        var xMove = head.x - tail.x;
+        xMove /= Math.Max(1, Math.Abs(xMove));
+        var yMove = head.y - tail.y;
+        yMove /= Math.Max(1, Math.Abs(yMove));
+        return new(tail.x + xMove, tail.y + yMove);
     }
 
-    public async ValueTask<long> GetPart2()
+    public static Point Move(Point point, char direction) => direction switch
     {
-        return 0;
-    }
+        'R' => point with { x = point.x + 1 },
+        'L' => point with { x = point.x - 1 },
+        'U' => point with { y = point.y + 1 },
+        'D' => point with { y = point.y - 1 },
+    };
 }
