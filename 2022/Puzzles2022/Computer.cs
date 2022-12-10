4f99645adf0e7909c3aef2b5a7902672cec3d8e5
Fred Morel <github@fmorel.net>
2022-12-10 09:15:11 -0500

    Extract Computer class, use clearer symbols

---
+++ 2022/Puzzles2022/Computer.cs
@@ -0,0 +1,109 @@
+﻿namespace Puzzles2022;
+
+public record Computer
+{
+    private const int SCREEN_WIDTH = 40;
+    private const int SCREEN_HEIGHT = 6;
+    private readonly List<Instruction> _instructions;
+
+    public Computer(string[] lines)
+    {
+        _instructions = lines.Select(x => ParseLine(x)).ToList();
+    }
+
+    public static Channel<long> CreateInputChannel() => Channel.CreateUnbounded<long>(new() { SingleReader = true, SingleWriter = true });
+    public static Channel<State> CreateScreenChannel() => Channel.CreateUnbounded<State>(new() { SingleReader = true, SingleWriter = true });
+
+    private static ChannelReader<long> GetCompletedInputReader()
+    {
+        var inputChannel = CreateInputChannel();
+        inputChannel.Writer.Complete();
+        return inputChannel.Reader;
+    }
+
+    private static Instruction ParseLine(ReadOnlySpan<char> line)
+    {
+        var op = Enum.Parse<Op>(line[0..4]);
+        return op switch
+        {
+            Op.noop => new Instruction(op, 0),
+            Op.addx => new Instruction(op, int.Parse(line[5..])),
+            _ => throw new UnreachableException(),
+        };
+    }
+
+    public async ValueTask Run(ChannelReader<long>? input, ChannelWriter<State> output)
+    {
+        input ??= GetCompletedInputReader();
+
+        // Write initial value to push output of other ops to *after* first cycle
+        var state = new State(1);
+        await output.WriteAsync(state);
+
+        foreach (var instruction in _instructions)
+        {
+            state = instruction.op switch
+            {
+                Op.noop => await Noop(output, state),
+                Op.addx => await AddX(output, state, instruction.value),
+                _ => throw new UnreachableException(),
+            };
+        }
+
+        output.Complete();
+    }
+
+    /// <summary>
+    /// 1 cycle, no changes
+    /// </summary>
+    private static async ValueTask<State> Noop(ChannelWriter<State> output, State state)
+    {
+        await output.WriteAsync(state);
+        return state;
+    }
+
+    /// <summary>
+    /// 2 cycles, add value to Register X
+    /// </summary>
+    private static async ValueTask<State> AddX(ChannelWriter<State> output, State state, long value)
+    {
+        await output.WriteAsync(state);
+        state = state with { X = state.X + value };
+        await output.WriteAsync(state);
+        return state;
+    }
+
+    public async IAsyncEnumerable<string> PrintScreen(ChannelReader<State> registers)
+    {
+        var output = new StringBuilder(SCREEN_WIDTH * SCREEN_HEIGHT + SCREEN_HEIGHT * 2);
+        while (await registers.WaitToReadAsync())
+        {
+            for (var y = 0; y < SCREEN_HEIGHT; y++)
+            {
+                for (var x = 0; x < SCREEN_WIDTH; x++)
+                {
+                    var value = (await registers.ReadAsync()).X;
+                    if (value == x + 1 || value == x - 1 || value == x)
+                        output.Append('⬜');
+                    else
+                        output.Append('⬛');
+                }
+
+                output.AppendLine();
+            }
+
+            yield return output.ToString();
+            await registers.ReadAsync();
+        }
+    }
+
+    public readonly record struct State(long X);
+
+    private record struct Instruction(Op op, int value);
+
+    private enum Op
+    {
+        noop,
+        addx,
+    }
+}
--- 2022/Puzzles2022/Day10/Solution.cs
+++ 2022/Puzzles2022/Day10/Solution.cs
@@ -1,88 +1,36 @@
 namespace Puzzles2022.Day10;
 
-public record Solution(List<Solution.Instruction> _lines) : ISolution<Solution>
+public record Solution(Computer cpu) : ISolution<Solution>
 {
     private const int WIDTH = 40;
     private const int HEIGHT = 6;
-    private readonly List<long> _x = new() { 1 };
 
-    public static Solution Init(string[] lines)
-    {
-        return new Solution(lines.Select(x => ParseLine(x)).ToList());
-    }
-
-    public static Instruction ParseLine(ReadOnlySpan<char> line)
-    {
-        var op = Enum.Parse<Op>(line[0..4]);
-        return op switch
-        {
-            Op.noop => new Instruction(op, 0),
-            Op.addx => new Instruction(op, int.Parse(line[5..])),
-        };
-    }
+    public static Solution Init(string[] lines) => new(new Computer(lines));
 
     public async ValueTask<long> GetPart1()
     {
-        long currentX = 1;
-        _x.Add(currentX);
-        foreach (var instruction in _lines)
+        var channel = Computer.CreateScreenChannel();
+        var cpuTask = cpu.Run(null, channel.Writer);
+
+        long sum = 0, cycle = 0;
+        await foreach (var value in channel.Reader.ReadAllAsync())
         {
-            _x.Add(currentX);
-            if (instruction.op == Op.noop)
-                continue;
-            else if (instruction.op == Op.addx)
-            {
-                currentX += instruction.value;
-                _x.Add(currentX);
-            }
+            cycle++;
+            if ((cycle - 20) % 40 == 0)
+                sum += value.X * cycle;
+
         }
 
-        var value = 0L;
-        for (var i = 20; i <= 220; i += 40)
-            value += _x[i] * i;
-        return value;
+        await cpuTask;
+        return sum;
     }
 
     public async ValueTask<string> GetPart2String()
     {
-        if (_x.Count == 1)
-            await GetPart1();
-        var output = new StringBuilder(WIDTH * HEIGHT + HEIGHT * 2);
-        for (var y = 0; y < HEIGHT; y++)
-        {
-            for (var x = 0; x < WIDTH; x++)
-            {
-                var cycle = x + (y * WIDTH) + 1;
-                var value = _x[cycle];
-                if (value == x + 1 || value == x - 1 || value == x)
-                    output.Append('#');
-                else
-                    output.Append('.');
-            }
-
-            output.AppendLine();
-        }
-
-        return output.ToString();
-    }
-    //
-    // public static long GetValueAtCycle(Dictionary<long, long> register, long cycle)
-    // {
-    //     while (cycle > 0)
-    //     {
-    //         if (register.TryGetValue(cycle, out long value))
-    //             return value;
-    //         cycle--;
-    //     }
-    //
-    //     return 1;
-    // }
-
-    public record struct Instruction(Op op, int value);
-
-    public enum Op
-    {
-        noop,
-        addx,
+        var channel = Computer.CreateScreenChannel();
+        var cpuTask = cpu.Run(null, channel.Writer);
+        var screens = await cpu.PrintScreen(channel.Reader).ToListAsync();
+        await cpuTask;
+        return screens[0];
     }
 }
